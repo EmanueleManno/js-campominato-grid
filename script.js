aeab7d4e7bd2e345d321029985a6d0cdb83d03fa
EmanueleManno <mannus1994@gmail.com>
2023-06-07 14:50:17 +0200

    Push pre-tutoring

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 console.log('JS OK');
 
 //RECUPERIAMO GLI ELEMENTI DAL DOM:
+const container = document.getElementsByClassName('container');
 const grid = document.getElementById('grid');
+const button = document.querySelector('button');
 
 //PREPARO I MIEI DATI INIZIALI:
 const rows = 10;
@@ -10,3 +12,8 @@ const cols = 10;
 const cells = rows * cols;
 
 //METTO IN ASCOLTO IL BOTTONE:
+button.addEventListener('click' , function(){
+    console.log('Si gioca');
+    //AL CLICK TOLTO LA CLASSE NONE AL CONTAINER:
+    container.classList.remove('none');
+})
