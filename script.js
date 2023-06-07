132eee8bf6bddfc67419637e2159390deaf6af59
EmanueleManno <mannus1994@gmail.com>
2023-06-07 14:29:14 +0200

    Abbellimento Header con CSS e classe None

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-//VEDO SE FUNZIONA
+//VEDO SE FUNZIONA:
 console.log('JS OK');
+
+//RECUPERIAMO GLI ELEMENTI DAL DOM:
+const grid = document.getElementById('grid');
+
+//PREPARO I MIEI DATI INIZIALI:
+const rows = 10;
+const cols = 10;
+const cells = rows * cols;
+
+//METTO IN ASCOLTO IL BOTTONE:
