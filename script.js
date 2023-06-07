cc6b0428c30af4cf0714737ec50a3ea0ff5b0cf4
EmanueleManno <mannus1994@gmail.com>
2023-06-07 17:18:10 +0200

    Aggiungo i numeri e il colore al click delle celle

--- script.js
+++ script.js
@@ -16,11 +16,30 @@ const rows = 10;
 const cols = 10;
 const cells = rows * cols;
 
+//FUNZIONE PER CREARE UNA CELLA:
+const createCell = () => {
+    const cell = document.createElement('div');
+    cell.className = 'cell'
+    return cell;
+}
+
+//RENDERIZZIAMO LE CELLE:
+for (let i=0; i< cells; i++) {
+    const cell = createCell();
+    cell.innerText = i +1;
+
+    //AL CLICK DELLA CELLA COLORALA DI AZZURRO:
+    cell.addEventListener('click',() => {
+        cell.classList.add('bg-lightblue');
+    })
+
+    grid.appendChild(cell);
+}
+
 //METTO IN ASCOLTO IL BOTTONE:
 button.addEventListener('click' , function(){
     console.log('Si gioca');
     //AL CLICK TOLTO LA CLASSE NONE AL CONTAINER:
     container.classList.remove('none');
 })
-
 
